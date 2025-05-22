98eb1298d4241b91154be00a3e34f240d43a3e6c
Abanta6 <dalexbanta95@gmail.com>
2025-05-22 01:06:46 -0500

    Terminal commands for retreiving data and completing assignment.

---
+++ answer.sql
@@ -0,0 +1,30 @@
+SELECT *
+FROM forum_posts
+WHERE
+  date BETWEEN '2048-04-01' AND '2048-05-01'
+  AND content ILIKE '%emptystack%'
+  AND content ILIKE '%dad%';
+
+  SELECT * FROM forum_accounts WHERE username = 'smart-money-44';
+
+  SELECT * FROM forum_accounts WHERE last_name = 'Steele';
+
+-- \q psql to enter node mainframe for emptystack download.
+
+SELECT *
+FROM emptystack_messages
+WHERE
+  body ILIKE '%taxi%';
+-- your-boss-99
+
+  SELECT *
+FROM emptystack_accounts
+WHERE username = 'your-boss-99';
+-- notagaincarter
+
+SELECT *
+FROM emptystack_projects
+WHERE code = 'TAXI';
+--DczE0v2b
+
+--node mainframe -stop + credentials
